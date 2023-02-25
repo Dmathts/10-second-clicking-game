ad6f7752428b513b60fe32dab970fa298b2fc011
Dmathts <126305745+Dmathts@users.noreply.github.com>
2023-02-24 22:32:11 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,12 +8,22 @@ input.onButtonPressed(Button.AB, function () {
         . . . # .
         `)
 })
+function R2D2 () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 5000, 1, 76, 0, 5000, SoundExpressionEffect.Warble, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
+}
 game.setScore(0)
 game.resume()
 basic.showString("Hello dante!")
 forever ;
 game.createSprite(2, 2)
 game.startCountdown(1000000)
+8*8+5
+control.waitForEvent(68, 70)
+basic.forever(function () {
+    while (game.score() < 25) {
+        R2D2()
+    }
+})
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
         game.addScore(1)
@@ -33,4 +43,3 @@ basic.forever(function () {
 control.inBackground(function () {
     basic.showIcon(IconNames.Yes)
 })
-8*8+5
